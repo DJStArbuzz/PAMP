cb710ba675a274694964b55754073af8d47f243d
Arbyz Svyatobyzovich <118758623+DJStArbuzz@users.noreply.github.com>
2026-03-14 12:34:00 +0300

    Update lyambda.hs

--- lyambda.hs
+++ lyambda.hs
@@ -1,5 +1,41 @@
+drop' :: Int -> [a] -> [a]
+drop' n xs = foldr(\x r k -> if k <= 0 then x : r (k - 1) else r (k - 1)) (const []) xs n
+
+dropWhile' :: (a -> Bool) -> [a] -> [a]
+dropWhile' p xs = foldr (\x r flag -> if flag && p x then r flag else x : r False) (const []) xs True
+
+reverse' :: [a] -> [a]
+reverse' = foldl(flip (:)) []
+
+product' :: (Num a) => [a] -> a 
+product' = foldl (*) 1
+
+mapNfilter :: (a -> b) -> (b -> Bool) -> [a] -> [b] 
+mapNfilter f p xs = foldr
+  (\x acc -> if p (f x) then f x:acc else acc)
+  [] xs
+
+filterNmap :: (a -> Bool) -> (a -> b) -> [a] -> [b]
+filterNmap p f xs = foldr
+  (\x acc -> if p x then f x:acc else acc)
+  [] xs
+
+map' :: (a -> b) -> [a] -> [b]
+map' f xs = foldr(\x acc -> f x:acc) [] xs
+
+map'' :: (a -> b) -> [a] -> [b]
+map'' f xs = foldl(\acc x -> acc ++ [f x]) [] xs
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' x xs = foldl
+  (\acc a -> if a == x then True else acc)
+  False xs
+
 sum' :: (Num a) => [a] -> a
 sum' xs = foldl (\acc x -> acc + x) 0 xs
+
+sum'' :: (Num a) => [a] -> a
+sum'' = foldl (+) 0
 
 flip' :: (a -> b -> c) -> b -> a -> c
 flip' f = \x y -> f y x
