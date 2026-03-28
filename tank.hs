e4cf4ac551e73d7e75aeb2be437f86092b61610a
Arbyz Svyatobyzovich <118758623+DJStArbuzz@users.noreply.github.com>
2026-03-28 12:18:11 +0300

    Update tank.hs

--- tank.hs
+++ tank.hs
@@ -14,3 +14,14 @@ setTankModel sTank nM = sTank (\(_, a, h) -> tank (nM, a, h))
 printTankInfo sTank = sTank(\(m, a, h) -> 
   "Model: " ++ m ++ ", Attack: " ++ (show a) ++ 
     ", Health: " ++ (show h))
+
+damage sTank dmg = sTank (\(m, a, h) ->
+  tank (m, a, max 0 (h - dmg)))
+
+fight aTank dTank = damage dTank attack
+  where attack = if getTankHealth aTank > 0
+                 then getTankAttack aTank
+                 else 0
+
+repair sTank repairAmount maxHp = sTank (\(m, a, h) ->
+  tank (m, a, min maxHp (h + repairAmount)))
